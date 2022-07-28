405465b2a3b8a3034bd6e4fd1b0df9d432621ebe
Shravani Nilange <shrawaninilange1999@gmail.com>
2022-07-28 21:43:38 +0530

    home

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import './Navbar.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <div className="nav">
+    <div className="left">
+    <h3 className='logo'>PathShala</h3>
+    </div>
+    <div className="right">
+            <button className='nn'>Sign In</button>
+            <button className='nn'>Sign Up</button>
+    </div>
+   </div>
   )
 }
 
